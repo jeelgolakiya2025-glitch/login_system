64b631660237a8adda831ee54936272c499c6ffb
unknown <jeelgolakiya2025@gmail.com>
2026-04-24 11:26:56 +0530

    update 2

--- BACKEND/models/product.model.js
+++ BACKEND/models/product.model.js
@@ -1,32 +1,58 @@
 const mongoose = require("mongoose");
 
-let ProductSchema = mongoose.Schema({
-  name: { 
-    type: String, 
-    required: true, 
-    minlength: 3 
-},
-  description: { 
-    type: String, required: true, minlength: 10 
-},
-  stock: { 
-    type: Number, required: true, default: 0 
-},
-  price: { 
-    type: String, required: true, default: null 
-},
-  discount: { 
-    type: Number, required: true, default: 0 
-},
-  isNewProduct: { 
-    type: Boolean, default: true 
-},
-  sku: { type: Number, unique: true, required: true },
-  images: [{ type: String, required: true }],
-  brand: { type: String, required: true },
-  category: { type: String, required: true, minlength: 3 },
-},
-{timestamps: true},
+let ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+    description: {
+      type: String,
+      required: true,
+      minlength: 10,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    discount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    isNewProduct: {
+      type: Boolean,
+      default: true,
+    },
+    sku: {
+      type: Number,
+      unique: true,
+      required: true,
+    },
+    images: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+  },
+  { timestamps: true },
 );
 
 module.exports = mongoose.model("product", ProductSchema);
