39ccdb7cf75c973baeeaec0eaa92146f537e9d16
jeelgolakiya2025-glitch <jeelgolakiya2025@gmail.com>
2026-04-25 12:32:13 +0530

    static and ai chatbot complit in backend

--- BACKEND/controllers/chat.controller.js
+++ BACKEND/controllers/chat.controller.js
@@ -15,3 +15,19 @@ module.exports.StaticBot = async (req, res) => {
         return res.status(400).json({message: error.message})
     }
 }
+
+module.exports.AIBot = async (req, res) => {
+    try {
+        const {message} = req.body;
+
+        if(!message){
+            return res.status(404).json({message:"can't get message"});
+        }
+
+        const reply = await chatService.AIBotReplay(message);
+
+        return res.status(200).json({reply});
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
--- BACKEND/routes/web/chat.route.js
+++ BACKEND/routes/web/chat.route.js
@@ -1,10 +1,14 @@
 // chatbot --> static(rule based Chatbot), ai chatbot (gemini key)
 const express = require("express");
 const router = express.Router();
-const chatController = require("../../controllers/chat.controller")
+const chatController = require("../../controllers/chat.controller");
+const userMiddleware = require("../../middlewares/user.middleware")
 
 // rule based Chatbot 
 router.post("/chat",chatController.StaticBot)
+
+// ai based Chatbot 
+router.post("/chat/ai", userMiddleware.authUser, chatController.AIBot)
 
 
 module.exports = router;
